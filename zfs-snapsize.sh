5536f54689cb58120e38c8133d2fd4c436714eba
Jan Przybylak <jp@j-p.space>
2020-12-09 21:56:15 +0100

    A size of 0B does not through an error message anymore

--- zfs-snapsize.sh
+++ zfs-snapsize.sh
@@ -48,22 +48,27 @@ fi
 # $2: Amount of significant digits
 si-format()
 {
-	# log(n)/log(10) is the 10th logarithm of the number. After rounding it down with int(), we get the number of digits in n
-	# Therefore, length(n)/3 gives us a factor f, which corresponds to the next smallest multiple of 1000, e.g.:
-	# 1; 10; 100             => f=0
-	# 1,000; 10,000; 100,000 => f=1
-	# 1,000,000; 10,000,000  => f=2
-	#
-	# Then, n is divided by 1000^f, giving us the first 1-3 digits.
-	# The printf call used here will output s significant digits.
-	#
-	# Finally, an SI-prefix corresponding to f is chosen.
-	#
-	awk -v n="$1" -v s="$2" '
-        	BEGIN { f=int( (log(n)/log(10)) / 3)
-        	printf("%." s "g"), n/(1024^f)
-        	printf substr("BKMGTP", f+1, 1) "\n"
-        	} '
+	if [ $1 = "0" ]
+	then
+		echo "0B"
+	else
+		# log(n)/log(10) is the 10th logarithm of the number. After rounding it down with int(), we get the number of digits in n
+		# Therefore, length(n)/3 gives us a factor f, which corresponds to the next smallest multiple of 1000, e.g.:
+		# 1; 10; 100             => f=0
+		# 1,000; 10,000; 100,000 => f=1
+		# 1,000,000; 10,000,000  => f=2
+		#
+		# Then, n is divided by 1000^f, giving us the first 1-3 digits.
+		# The printf call used here will output s significant digits.
+		#
+		# Finally, an SI-prefix corresponding to f is chosen.
+		#
+		awk -v n="$1" -v s="$2" '
+			BEGIN { f=int( (log(n)/log(10)) / 3)
+			printf("%." s "g"), n/(1024^f)
+			printf substr("BKMGTP", f+1, 1) "\n"
+			} '
+	fi
 }
 
 
