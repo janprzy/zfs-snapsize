23c6743d7de3084c04bc46072a8afbdad2df5661
Jan Przybylak <jp@j-p.space>
2021-04-29 20:52:38 +0200

    Added slash

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Install this script to /usr/local/bin
+# Install this script to /usr/local/bin/
 
 echo "Installing ./znapsize.sh to /usr/local/bin/znapsize..."
 sudo cp -vi ./znapsize.sh /usr/local/bin/znapsize
